46d5ede2719556381f7988ee3b1da533d827e59b
Yuxin <ychen@luc.edu>
2020-12-02 16:46:55 -0800

    some initial tweaks and project organization

--- web-service/core.py
+++ web-service/core.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -9,3 +9,9 @@ if __name__ == "__main__":
 @app.route("/")
 def hello_world():
     return "Hello World!"
+
+@app.route("/api/v1/devices/<device_id>", methods={"GET", "POST"})
+def get_device_info(device_id):
+    if request.method == "GET":
+        return "GET %s" % device_id
+    return "POST %s" % device_id

f85ac13ddeb787f58de2d93ed57b1dfad79a8a40
Yuxin Chen <yuxinchen0818@gmail.com>
2020-12-11 08:15:47 +0000

    fixed pi client

--- client/light.py
+++ client/light.py
@@ -1,15 +1,23 @@
 from gpiozero import LightSensor, Buzzer
 import time
+import requests
+import network_lib
 
 ldr = LightSensor(4)
 buzzer = Buzzer(17)
 
+device_name = 'Garden'
+
+my_id = {'device_id': device_name}
+create_requests = requests.post(network_lib.construct_url("/api/v1/device/create"), json=my_id)
+network_lib.send_request_async(network_lib.start_reporting_heartbeat, device_name)
 while True:
     print(ldr.value)
-    time.sleep(0.1)
     if ldr.value < 0.5:
+        network_lib.send_request_async(network_lib.report_incident, device_name)
         buzzer.on()
     else:
         buzzer.off()
+    time.sleep(0.1)
 
 
--- client/network_lib.py
+++ client/network_lib.py
@@ -1,5 +1,20 @@
 import requests
+import time
+import threading
+import urllib.parse
 
-info = {'id':'pi', 'status':'connect', 'event':'OK'}
-response = requests.post('http://192.168.0.16/api/v1/devices/device_info',
-                        json= info )
+server_prefix = "http://34.94.27.195:2000/"
+
+def construct_url(api):
+    return urllib.parse.urljoin(server_prefix, api)
+
+def send_request_async(func, device_name):
+    threading.Thread(target=func, args=(device_name,)).start()
+
+def report_incident(device_name):
+    requests.post(construct_url("/api/v1/device/incident"), json={"device_id": device_name})
+
+def start_reporting_heartbeat(device_name):
+    while True:
+        requests.post(construct_url("/api/v1/device/ping"), json={"device_id": device_name})
+        time.sleep(0.5)
